7164c720b18ce63e28d09886919209b2f9c204c8
kppetrick <kppetrick@gmail.com>
2020-11-24 16:04:42 -0500

    made my lab

---
+++ .idea/src/main/java/Music.java
@@ -0,0 +1,11 @@
+public class Music {
+
+        private String[] playList;
+        public Music(String[] playList){
+            this.playList = playList;
+        }
+        public Integer selection(Integer startIndex, String selection){
+            return null;
+        }
+    }
+
---
+++ .idea/src/test/java/TestMusic.java
@@ -0,0 +1,25 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+public class TestMusic {
+    @Test
+    public void selectionTest1(){
+        String[] playlist = {"wheniseeyouagain","borntorun","nothingelsematters","cecelia"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 1;
+        String selection = "cecelia";
+        Integer expected = 2;
+        Integer actual = music.selection(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void selectionTest2(){
+        String[] playlist = {"dancinginthedark","rio","liveoak","liveoak"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 0;
+        String selection = "liveoak";
+        Integer expected = 1;
+        Integer actual = music.selection(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+}
